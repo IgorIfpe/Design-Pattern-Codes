21497953723aaaf046e5a85cd1ffc2775f39da53
Igor Santos <ijrs1@discente.ifpe.edu.br>
2022-11-21 21:34:48 -0300

    template method

---
+++ src/data_mining/DocumentTemplate.java
@@ -0,0 +1,17 @@
+package data_mining;
+
+public abstract class DocumentTemplate {
+	
+	abstract void openFile();
+	abstract void closeFile();
+	abstract void extract();
+	abstract void convertFile();
+	
+	public void analyzeData() {
+		System.out.println("Data was analyzed !");
+	}
+	
+	public void sendReports() {
+		System.out.println("Reports was sended.");
+	}
+}
